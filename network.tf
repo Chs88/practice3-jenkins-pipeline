15e6463a55b19c989e68059d509b23da041f901f
Chs88 <faskertikr@gmail.com>
2022-03-31 03:53:34 +0100

    added temprorary network config, just to text. Network will look different

---
+++ network.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main-subnet"
+  }
+}
+
