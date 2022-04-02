4bf3a5fe3c62a06ca208731a3456c22d379b0e1b
Chs88 <faskertikr@gmail.com>
2022-04-02 04:32:47 +0100

    Added Jenkins server resource + changed naming for Project_name. Everything uses a variable now which can be defined in the tfvars file as. It is currently Project3

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ module "web_server" {
   
   source = "./modules/ec2"
 
+  project_name = var.project_name
+
   infra_env = var.infra_env
 
   ec2_role = "web_server"
@@ -32,6 +34,8 @@ module "database" {
 
   source = "./modules/ec2"
 
+  project_name = var.project_name
+
   infra_env = var.infra_env
 
   ec2_role = "database"
@@ -51,6 +55,8 @@ module "vpc" {
 
   source = "./modules/vpc"
 
+  project_name = var.project_name
+
   infra_env = var.infra_env
 
   vpc_cidr = "192.0.0.0/17" ##I am going to half the default amount of IPs for now
@@ -59,7 +65,29 @@ module "vpc" {
 }
 
 
+##Jenkins Server resource
 
+module "jenkins_server" {
+
+  source = "./modules/ec2"
+
+  project_name = var.project_name
+
+  infra_env = var.infra_env
+
+  ec2_role = "jenkins_server"
+
+  subnet = module.vpc.vpc_private_subnet
+
+  create_eip = false
+
+  security_groups = ["${module.vpc.security_group_private}"]
+
+  # user_data = file("./scripts/database.sh")
+
+  associate_public_ip_address = true
+  
+}
 
 ## Going to use a little workaround to provision the private key to the webserver
 # Using a null_resource will still trigger the provisioning 
